1c73c5d4b75a7de75516a907ff2ab9a5bdd91d6e
vinayvenkatesh08 <vinayvenkatesh08@gmail.com>
2019-12-13 09:56:43 -0500

    Updated Independednt CSV Parser, Reads CSV into 2D Array and extracts sensor readings for each tiem stamps and store it in a 1D Array

--- src/csv_parser.c
+++ src/csv_parser.c
@@ -1,70 +1,187 @@
-/* CSV Parserr: Reads csv file and returns a vector with all sensor readings for a particular time stamp where first element of array is the time stamp followed by readings */
-
 #include <stdio.h>
-#include <string.h>
+#include <conio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("incorrect number of arguments, send csv file as argument and delimiter\n");
-        return -1;
+// helper function to get file size
+int getfilesize(char *fname)
+{
+  FILE *fp = fopen(fname, "r");
+  fseek(fp, 0, SEEK_END);
+  int size = ftell(fp);
+  fclose(fp);
+  return size;
+}
+ 
+// reads csv file into a local buffer
+// tokenize string at comma or new line, 
+// convert text data to integer and write it into 2D buffer
+#define COL 3
+void read2darray(char *fname, float (*array)[COL], int rows)
+{
+  // allocate memory buffer for file read 
+  // and read csv file into buffer
+  int size = getfilesize(fname);
+  char *buf = malloc(size +1);
+ 
+  FILE *fp = fopen(fname, "r");
+  fread(buf, size, 1, fp);
+  fclose(fp) ;
+  
+  //tokenize at comma or new line
+  char *tok = strtok(buf, ",\n");
+  for(int i=0;i<rows;i++)
+  {
+    for(int j=0;j<COL;j++)
+    {
+      if(tok == NULL)
+      {
+        break;
+      }
+      // convert text numeral to integer
+      float val = atof(tok);
+      array[i][j] = val;
+      //printf("%d ", val);
+ 
+      // tokenize at comma or newline
+      tok = strtok(NULL, ",\n");
     }
-    printf("filename=%s, delimiter=%s\n", argv[1], argv[2]);
-
-    FILE *fp = fopen(argv[1], "r");
-    if (!fp) {
-        printf("Can't open file\n");
-        return 0;
+    
+  }
+  free(buf);
+}
+ 
+// dummy function to create csv file and 
+// write comma seperated sequence as 4x6 array (0,1,2..,22,23) 
+void writedata(char *fname, int rows)
+{
+  FILE *fp = fopen(fname, "w");
+  int index = 0;
+  for(int i=0; i< rows; i++)
+  {
+    fprintf(fp, "%f,%f,%f,%f\n", index, index+1, index+2, index+3, index+4, index+5);
+    index += COL;
+  }
+  fclose(fp);
+}
+// #define ROWS 10
+int main()
+{
+  int count=0;
+  char line[1024];
+  FILE *lp;
+  
+  
+  lp = fopen("sample.csv", "r");
+    while (fgets(line, 1024, lp )) {
+        count++;
     }
-
-    char *delimiter = argv[2];
-    printf("delimiter=%s\n", delimiter);
-
-    char buf[1024];
-    char *arr = (char*)calloc(1, 1024);
-    char time_stamp_buf[1024] = { 0 };
-    int row_count = 0;
-    int field_count = 0;
-    while (fgets(buf, 1024, fp)) {
-        field_count = 0;
-        row_count++;
-
-        if (row_count == 1) {
-            continue;
-        }
-
-        char *field = strtok(buf, delimiter);
-        while (field) {
-            if (field_count == 0) {
-                if (strcmp(time_stamp_buf, "0")) {
-                    printf("Time Stamp not 0\t");
-                    if (!strcmp(field, time_stamp_buf)) {
-                        printf("time stamp same, increase memory of row");
-                        arr = realloc(arr, sizeof(arr)+sizeof(buf));
-                        printf("array size %lu\n", sizeof(arr));
-                    }
-                }
-                strcpy(time_stamp_buf, field);
-            }
-            if (field_count == 1) {
-                //printf("Sensor:\t");
-            }
-            if (field_count == 2) {
-                    //printf("Reading:\t");
-            }
-            if (field_count == 0) {
-                printf("Time Stamp:\t");
-                printf("%s\t", field);
-            }
-            //printf("%s\t", field);
-            if (field_count == 0)
-            	printf("Time Stamp buf:%s\t", time_stamp_buf);
-
-            field = strtok(NULL, ",");
-            field_count++;
-        }
-        printf("\n");
+	// printf("Rows is %d", count);
+	int ROWS = count;
+  float arr[ROWS][COL];
+  for(int i=0;i<ROWS;i++)
+  {
+    for(int j=0;j<COL;j++)
+    {
+      arr[i][j]=0;
     }
-    fclose(fp);
-    return 0;
+  }
+  char *fname = "sample.csv";
+ 
+  // write some dummy data into csv file
+  // it's not needed if you already have csv file
+  // writedata(fname, ROWS);
+ 
+  // read file into 2D into array
+  read2darray(fname, (float(*)[COL])arr, ROWS);
+  printf("\nCSV File is:\n");
+ 
+  // print 2d array
+  for(int i=0;i<ROWS;i++)
+  {
+    for(int j=0;j<COL;j++)
+    {
+      printf("%f ", arr[i][j]);
+    }
+    printf("\n");
+  }
+
+   float result1[10] = {0};  
+   float result2[10] = {0};
+   float result3[10] = {0};
+   float result4[10] = {0};
+   int m=0;
+   int n=0;
+   int o=0;
+   int p=0;
+   
+   
+   for(int i=0;i<ROWS;i++)
+  {
+	  if (arr[i][0]== arr[0][0])
+	  {
+		  result1[i] = arr[i][2];
+		  //printf("%f is %f \n\n", arr[i][0],result1[i]);
+		  m++;  
+	  }
+	  else break;
+  }
+  
+    for(int i=0;i<ROWS;i++)
+  {
+	  if (arr[i][0]== arr[0][0])
+	  {
+		  result1[i] = arr[i][2];
+		  //printf("%f is %f \n\n", arr[i][0],result1[i]);
+		  m++;  
+	  }
+	  else break;
+  }
+  
+	  if (arr[i][0]== arr[m][0])
+	  {
+		  result2[n] = arr[i][2];
+		  //printf("%f is %f \n\n", arr[i][0],result2[i]);
+		  n++;
+	  }
+      else if (arr[i][0]== arr[m+n][0])
+	  {
+		  result3[o] = arr[i][2];
+		  //printf("%f is %f \n\n", arr[i][0],result3[i]);
+		  o++;
+	  }
+	  else if (arr[i][0]== arr[m+n+o][0])
+	  {
+		  result4[p] = arr[i][2];
+		  //printf("%f is %f \n\n", arr[i][0],result4[i]);
+		  p++;
+	  }
+	  else break; 
+  }
+  
+  printf("\nSensor readings for timestamp1 stored in result1 array is\n");
+  for(int i=0;i<m;i++)
+  {
+	  printf("%f \n",result1[i]);
+  }
+  
+  printf("\nSensor readings for timestamp2 stored in result2 array is\n");
+  for(int i=0;i<n;i++)
+  {
+	  printf("%f \n",result2[i]);
+  }
+  
+  printf("\nSensor readings for timestamp3 stored in result3 array is\n");
+  for(int i=0;i<o;i++)
+  {
+	  printf("%f \n",result3[i]);
+  }
+  
+  printf("\nSensor readings for timestamp4 stored in result4 array is\n");
+  for(int i=0;i<p;i++)
+  {
+	  printf("%f \n",result4[i]);
+  }
+  
+  
 }
